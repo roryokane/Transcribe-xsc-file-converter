de4cf0691b40f3dcf888c76b25104fcbdb3a08fe
Rory O’Kane <rory@roryokane.com>
2020-04-03 16:53:24 -0400

    Replace Baretest with Jest
    in preparation for adding a snapshot test

--- parser.test.ts
+++ parser.test.ts
@@ -1,14 +1,10 @@
-import * as baretest from "baretest"
-import * as assert from "assert"
 import { initialParseState } from "./parser"
 import { lineHandlers } from "./line_handlers"
-
-const test = baretest("Parser")
 
 test("handleMarker", () => {
   const initialState = { ...initialParseState, currentSection: "Markers" }
   const newState = lineHandlers.B("B,176400,1,,0,0:00:04.000", initialState)
-  assert.deepStrictEqual(newState.data.markers.list, [
+  expect(newState.data.markers.list).toEqual([
     {
       label: null,
       markerType: {
@@ -29,6 +25,3 @@ test("handleMarker", () => {
 })
 
 // TODO write fixture test
-// (probably requires Jest)
-
-test.run()
