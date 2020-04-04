6d98d493547a1522b401c4ec5ae349e7d63800b1
Rory O’Kane <rory@roryokane.com>
2020-04-04 16:00:33 -0400

    Fix hard-coded error message of assertCurrentSection

--- parsing/line_parsers.ts
+++ parsing/line_parsers.ts
@@ -26,7 +26,9 @@ function parseSectionEnd(line: string, currentState: ParseState): ParseState {
 
 function assertCurrentSection(expectedSection: string, currentState: ParseState): void {
   if (currentState.currentSection !== expectedSection) {
-    throw new Error("found marker data key outside of Markers section; change code to support that")
+    throw new Error(
+      `found a field expected to be in the ${expectedSection} section in the ${currentState.currentSection} section instead; change code to support that`
+    )
   }
 }
 
